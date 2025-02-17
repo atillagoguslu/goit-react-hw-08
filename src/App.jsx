27230f9ab3791d3eec76e6c30e9aff469e4093e1
AtillaGoguslu <atillagoguslu@hotmail.com>
2025-02-17 19:34:23 +0300

    Implement routing and lazy loading for application pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import ContactList from "./components/ContactList.jsx";
-import SearchBox from "./components/SearchBox.jsx";
-import ContactForm from "./components/ContactForm.jsx";
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router";
+
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const RegisterPage = lazy(() => import("./pages/register/RegistrationPage.jsx"));
+const LoginPage = lazy(() => import("./pages/login/LoginPage.jsx"));
+const ContactsPage = lazy(() => import("./pages/contacts/ContactsPage.jsx"));
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="phonebook-title">Phonebook with MockAPI</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import "./reset.css";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router"; // Without "-dom" new library
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
